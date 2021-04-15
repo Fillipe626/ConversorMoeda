3a986e404d12ce86b7bd545e68bcd965010cc96e
Fillipe626 <lpjp91@gmail.com>
2021-04-15 15:38:57 -0400

    Criada uma função para enxugar a criação de text field

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,6 @@ import 'dart:convert';
 const request = "https://api.hgbrasil.com/finance?format=json-cors&key=b3232927";
 
 void main() async {
-
-
-
   runApp(MaterialApp(
     home: Home(),
     theme: ThemeData(
@@ -24,7 +21,6 @@ void main() async {
         )
     ),
   ));
-  
 }
 
 Future<Map> getData() async {
@@ -40,92 +36,78 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   double dollar;
   double euro;
-  
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text("Conversor de \$ moedas \$ "),
-        backgroundColor: Colors.amber,
-        centerTitle: true,
-      ),
-      body: FutureBuilder<Map>(//constroi a tela a depender do get data, a depender dos dados futuros
-        future: getData(),
-        builder: (context, snapshot){
-          switch(snapshot.connectionState){//Estado da conexao
-            case ConnectionState.none:
-            case ConnectionState.waiting:
-              return Center(
-                child: Text("Carregando dados...",
-                  style: TextStyle(
-                    color: Colors.amber,
-                    fontSize: 25.0),
-                    textAlign: TextAlign.center,)
-              );
-            default: if(snapshot.hasError){
-              return Center(
-                  child: Text("Erro ao carregar dados..",
-                  style: TextStyle(
-                  color: Colors.amber,
-                  fontSize: 25.0),
-          textAlign: TextAlign.center,)
-              );
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text("Conversor de \$ moedas \$ "),
+          backgroundColor: Colors.amber,
+          centerTitle: true,
+        ),
+        body: FutureBuilder<
+            Map>( //constroi a tela a depender do get data, a depender dos dados futuros
+            future: getData(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) { //Estado da conexao
+                case ConnectionState.none:
+                case ConnectionState.waiting:
+                  return Center(
+                      child: Text("Carregando dados...",
+                        style: TextStyle(
+                            color: Colors.amber,
+                            fontSize: 25.0),
+                        textAlign: TextAlign.center,)
+                  );
+                default:
+                  if (snapshot.hasError) {
+                    return Center(
+                        child: Text("Erro ao carregar dados..",
+                          style: TextStyle(
+                              color: Colors.amber,
+                              fontSize: 25.0),
+                          textAlign: TextAlign.center,)
+                    );
+                  } else {
+                    dollar =
+                    snapshot.data["results"]["currencies"]["USD"]["buy"];
+                    euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
 
-            }else {
-              dollar = snapshot.data["results"]["currencies"]["USD"]["buy"];
-              euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                    return SingleChildScrollView(
+                        padding: EdgeInsets.all(10.0),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: <Widget>[
+                            Icon(Icons.monetization_on, size: 150.0,
+                            color: Colors.amber),
+                        buildTextField("Reais", "R\$"),
+                        Divider(),
+                        buildTextField("Dólares", "US\$"),
+                        Divider(),
+                        buildTextField("Euros", "\€")
 
-              return SingleChildScrollView(
-                padding: EdgeInsets.all(10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Icon(Icons.monetization_on,size: 150.0, color: Colors.amber),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: "Reais",
-                        labelStyle: TextStyle(color: Colors.amber),
-                        border: OutlineInputBorder(),
-                        prefixText: "R\$"
-                      ),
-
-                      style: TextStyle(
-                        color: Colors.amber, fontSize: 25.0
-                      ),
-                    ),
-                    Divider(),
-                    TextField(
-                      decoration: InputDecoration(
-                          labelText: "Dolares",
-                          labelStyle: TextStyle(color: Colors.amber),
-                          border: OutlineInputBorder(),
-                          prefixText: "US\$"
-                      ),
-                      style: TextStyle(
-                          color: Colors.amber, fontSize: 25.0
-                      ),
-                    ),
-                    Divider(),
-                    TextField(
-                      decoration: InputDecoration(
-                          labelText: "Euros",
-                          labelStyle: TextStyle(color: Colors.amber),
-                          border: OutlineInputBorder(),
-                          prefixText: "\€"
-                      ),
-                      style: TextStyle(
-                          color: Colors.amber, fontSize: 25.0
-                      ),
-                    )
-                  ],
+                    ],
                 ),
-              );
+               );
             }
-
-          }
-        })
+            }
+            })
     );
   }
+}
+
+Widget buildTextField(String label, String prefix) {
+  return TextField(
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.amber),
+        border: OutlineInputBorder(),
+        prefixText: prefix
+    ),
+    style: TextStyle(
+        color: Colors.amber, fontSize: 25.0
+    ),
+  );
 }
